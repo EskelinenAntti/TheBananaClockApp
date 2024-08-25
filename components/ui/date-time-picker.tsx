06bc653bcc684163bb974194d8ecfbe9d262c046
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2024-08-25 21:05:00 +0300

    Fix date selector on mobile

--- components/ui/date-time-picker.tsx
+++ components/ui/date-time-picker.tsx
@@ -7,12 +7,15 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { TimePicker } from "./time-picker";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogTrigger,
+} from "./dialog";
+import { CheckIcon } from "@radix-ui/react-icons";
 
 interface DateTimePickerProps {
   date: Date;
@@ -38,8 +41,8 @@ export function DateTimePicker({ date, setDate, id }: DateTimePickerProps) {
   };
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button
           id={id}
           variant={"outline"}
@@ -51,18 +54,25 @@ export function DateTimePicker({ date, setDate, id }: DateTimePickerProps) {
           <CalendarIcon className="mr-2 h-4 w-4" />
           {date ? format(date, "PPP HH:mm:ss") : <span>Pick a date</span>}
         </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
+      </DialogTrigger>
+      <DialogContent className="w-auto p-0" showCloseButton={false}>
         <Calendar
           mode="single"
           selected={date}
           onSelect={(d) => handleSelect(d)}
           initialFocus
         />
-        <div className="p-3 border-t border-border">
+        <div className="pt-3 px-3 border-t border-border">
           <TimePicker setDate={(date) => date && setDate(date)} date={date} />
         </div>
-      </PopoverContent>
-    </Popover>
+        <DialogFooter className="pb-4 px-4">
+          <DialogClose asChild>
+            <Button type="submit">
+              Done <CheckIcon />
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
